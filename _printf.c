95d59b8ac42d8e6033cbaec975f6b0cc4d902fdb
root <chichakabdullayeva>
2024-12-01 19:06:40 +0000

    done

--- _printf.c
+++ _printf.c
@@ -9,32 +9,25 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0;
-	unsigned int len = 0;
-	
-	va_start(args, format);
-	while (format[i])
-	{
-		if (format[i] == '%' && format[i + 1] == 'c')
-			len += _putchar(va_arg(args, int)), i += 2;
-
-		else if (format[i] == '%' && format[i + 1] == 's')
-			len += print_string(va_arg(args, char *)), i += 2;
-
-		else if (format[i] == '%' && format[i + 1] == '%')
-			len += _putchar('%'), i += 2;
-
-		else if (format[i] == '%' && format[i + 1] == '\0')
-			len--, i++;
-
-		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
-			len += print_number(va_arg(args, int)), i += 2;
-
-		else
-			len += _putchar(format[i]), i++;
-	}
-	va_end(args);
-	return (len);
+va_list args;
+int i = 0;
+unsigned int len = 0;
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%' && format[i + 1] == 'c')
+len += _putchar(va_arg(args, int)), i += 2;
+else if (format[i] == '%' && format[i + 1] == 's')
+len += print_string(va_arg(args, char *)), i += 2;
+else if (format[i] == '%' && format[i + 1] == '%')
+len += _putchar('%'), i += 2;
+else if (format[i] == '%' && format[i + 1] == '\0')
+len--, i++;
+else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+len += print_number(va_arg(args, int)), i += 2;
+else
+len += _putchar(format[i]), i++;
 }
-
+va_end(args);
+return (len);
+}
--- print_number.c
+++ print_number.c
@@ -8,44 +8,38 @@
  */
 int print_number(int num)
 {
-	int len = 1;
-	int ulen = 1;
-	int i = 0;
-	int numcpy;
-	int divisor = 1;
-
-	if (num == INT_MIN)
-	{
-		num = 147483648;
-		_putchar('-');
-		_putchar('2');
-		len += 2;
-	}
-
-	if (num < 0)
-	{
-		_putchar('-');
-		num *= -1;
-		len++;
-	}
-
-	numcpy = num;
-
-	while (numcpy >= 10)
-	{
-		numcpy /= 10;
-		len++;
-		ulen++;
-		divisor *= 10;
-	}
-
-	while (i < ulen)
-	{
-		_putchar('0' + num / divisor);
-		num %= divisor;
-		divisor /= 10;
-		i++;
-	}
-
-	return (len);
+int len = 1;
+int ulen = 1;
+int i = 0;
+int numcpy;
+int divisor = 1;
+if (num == INT_MIN)
+{
+num = 147483648;
+_putchar('-');
+_putchar('2');
+len += 2;
+}
+if (num < 0)
+{
+_putchar('-');
+num *= -1;
+len++;
+}
+numcpy = num;
+while (numcpy >= 10)
+{
+numcpy /= 10;
+len++;
+ulen++;
+divisor *= 10;
+}
+while (i < ulen)
+{
+_putchar('0' + num / divisor);
+num %= divisor;
+divisor /= 10;
+i++;
+}
+return (len);
 }
